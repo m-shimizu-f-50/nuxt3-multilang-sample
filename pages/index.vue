c6f89148bcc94c8217dbfcc75c30931d2dea6ac5
simizu-masato <m.shimizu@trigger4.com>
2025-02-16 22:39:11 +0900

    セットアップ

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup>
+const { setLocale } = useI18n();
+</script>
+
+<template>
+	<div>
+		<div>
+			<button @click="setLocale('en')">en</button>
+			<button @click="setLocale('fr')">fr</button>
+			<p>{{ $t('welcome') }}</p>
+		</div>
+	</div>
+</template>
